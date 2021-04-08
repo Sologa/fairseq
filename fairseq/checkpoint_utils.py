fb3a55da0f03b646545b363ac94c9b529606fc37
Sologa <b04901114@ntu.edu.tw>
2021-04-08 16:05:59 +0800

    Update checkpoint_utils.py

--- fairseq/checkpoint_utils.py
+++ fairseq/checkpoint_utils.py
@@ -61,14 +61,16 @@ def save_checkpoint(cfg: CheckpointConfig, trainer, epoch_itr, val_loss):
 
     suffix = trainer.checkpoint_suffix
     checkpoint_conds = collections.OrderedDict()
-    checkpoint_conds["checkpoint{}{}.pt".format(epoch, suffix)] = (
-        end_of_epoch and not cfg.no_epoch_checkpoints and epoch % cfg.save_interval == 0
-    )
-    checkpoint_conds["checkpoint_{}_{}{}.pt".format(epoch, updates, suffix)] = (
-        not end_of_epoch
-        and cfg.save_interval_updates > 0
-        and updates % cfg.save_interval_updates == 0
-    )
+    checkpoint_conds["checkpoint{}{}.pt".format(epoch, suffix)] = (False)
+    checkpoint_conds["checkpoint_{}_{}{}.pt".format(epoch, updates, suffix)] = (False)
+#     checkpoint_conds["checkpoint{}{}.pt".format(epoch, suffix)] = (
+#         end_of_epoch and not cfg.no_epoch_checkpoints and epoch % cfg.save_interval == 0
+#     )
+#     checkpoint_conds["checkpoint_{}_{}{}.pt".format(epoch, updates, suffix)] = (
+#         not end_of_epoch
+#         and cfg.save_interval_updates > 0
+#         and updates % cfg.save_interval_updates == 0
+#     )
     checkpoint_conds["checkpoint_best{}.pt".format(suffix)] = val_loss is not None and (
         not hasattr(save_checkpoint, "best")
         or is_better(val_loss, save_checkpoint.best)
