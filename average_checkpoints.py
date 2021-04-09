6c77e2c691af24462a4e94b7b7eb7ca341e5f3cf
Sologa <b04901114@ntu.edu.tw>
2021-04-09 23:02:59 +0800

    Update average_checkpoints.py

--- average_checkpoints.py
+++ average_checkpoints.py
@@ -77,7 +77,7 @@ def last_n_checkpoints(paths, n, update_based, upper_bound=None):
     assert len(paths) == 1
     path = paths[0]
     if update_based:
-        pt_regexp = re.compile(r'checkpoint\.best_bleu_(\d+\.?\d*)\.pt')
+        pt_regexp = re.compile(r'checkpoint\.best_bleu_(\d+\.?\d*){}\.pt')
         # pt_regexp = re.compile(r'checkpoint_\d+_(\d+)\.pt') 
     else:
         pt_regexp = re.compile(r"checkpoint(\d+)\.pt")
