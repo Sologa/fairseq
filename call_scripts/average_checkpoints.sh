ab9b67319c8fd1388a8b9d1df2c7d3f87976af50
Sologa <b04901114@ntu.edu.tw>
2021-04-11 23:01:15 +0800

    Delete call_scripts directory

--- call_scripts/average_checkpoints.sh
+++
@@ -1,5 +0,0 @@
-CHECKPOINT_DIR=checkpoints_dir/ctc-intermediate_loss-cutoff-ffn_2048/
-python average_checkpoints.py \
-    --inputs $CHECKPOINT_DIR \
-    --output $CHECKPOINT_DIR/checkpoint_best.pt \
-    --num-update-checkpoints 5 \
--- call_scripts/call_test.sh
+++
@@ -1,5 +0,0 @@
-CHECKPOINT_DIR=checkpoints_dir/ctc-intermediate_loss-cutoff-ffn_2048/
-python generate.py ../data-bin/iwslt14.tokenized.de-en-distilled-cutoff \
-    --path $CHECKPOINT_DIR/checkpoint_best.pt \
-    --batch-size 128 --beam 5 --remove-bpe \
-    --quiet \
